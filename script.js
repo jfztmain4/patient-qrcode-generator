ac9c4b7d3fae1ac4fad4c9f55b6e5e35a296c981
jfztmain4 <joefrancistorquator@gmail.com>
2025-02-06 16:47:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,35 @@ function generateQRCode() {
         weight: form.weight.value,
         gender: form.gender.value
     };
-    var qrCodeData = JSON.stringify(data);
 
-    // Clear previous QR code if any
-    var qrcodeContainer = document.getElementById("qrcode");
-    qrcodeContainer.innerHTML = "";
+    // Prompt user to confirm information
+    var confirmationMessage = `
+        Please confirm your information:\n
+        Name: ${data.name}\n
+        Age: ${data.age}\n
+        Address: ${data.address}\n
+        Contact Number: ${data.contact}\n
+        Weight: ${data.weight}\n
+        Gender: ${data.gender}
+    `;
+    if (confirm(confirmationMessage)) {
+        var qrCodeData = JSON.stringify(data);
 
-    // Generate new QR code
-    var qrCode = new QRCode(qrcodeContainer, {
-        text: qrCodeData,
-        width: 128,
-        height: 128
-    });
+        // Clear previous QR code if any
+        var qrcodeContainer = document.getElementById("qrcode");
+        qrcodeContainer.innerHTML = "";
+
+        // Generate new QR code
+        var qrCode = new QRCode(qrcodeContainer, {
+            text: qrCodeData,
+            width: 128,
+            height: 128
+        });
+
+        // Scroll to QR code
+        qrcodeContainer.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    } else {
+        // Allow user to edit their information
+        alert("Please correct your information and try again.");
+    }
 }
